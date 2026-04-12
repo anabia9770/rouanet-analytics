376101e518870f6acfa0e6452139a243c4f07040
anabia9770 <beatriz302.correa@gmail.com>
2026-04-12 15:20:16 -0300

    Update components.py

--- components.py
+++ components.py
@@ -1,9 +1,23 @@
-import streamlit as st
-
-def card(titulo, valor):
+def card(title, value, icon):
     st.markdown(f"""
         <div class="card">
-            <h4 style="color:#6B7280; font-size:14px;">{titulo}</h4>
-            <h2 style="color:#111827;">{valor}</h2>
+            <div style="display:flex; justify-content:space-between; align-items:center;">
+                <div>
+                    <div class="kpi-title">{title}</div>
+                    <div class="kpi-value">{value}</div>
+                </div>
+                <div style="
+                    width:40px;
+                    height:40px;
+                    border-radius:50%;
+                    background:#ede9fe;
+                    display:flex;
+                    align-items:center;
+                    justify-content:center;
+                    font-size:18px;
+                ">
+                    {icon}
+                </div>
+            </div>
         </div>
     """, unsafe_allow_html=True)
