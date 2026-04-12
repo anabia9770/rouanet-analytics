3583ce53492230db7c1a79f1b6de13b1a21a7337
anabia9770 <beatriz302.correa@gmail.com>
2026-04-12 15:29:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -113,15 +113,37 @@ if seg: df_f = df_f[df_f["segmento"].isin(seg)]
 c1, c2, c3, c4 = st.columns(4)
 
 def card(title, value, icon):
-    return f"""
-    <div class="card">
-        <div style="display:flex; justify-content:space-between;">
-            <div class="kpi-title">{title}</div>
-            <div>{icon}</div>
+    st.markdown(f"""
+    <div style="
+        background:white;
+        padding:20px;
+        border-radius:16px;
+        box-shadow:0 4px 20px rgba(0,0,0,0.05);
+        display:flex;
+        justify-content:space-between;
+        align-items:center;
+        height:110px;
+    ">
+        <div>
+            <div style="font-size:14px; color:#6b7280;">
+                {title}
+            </div>
+            <div style="font-size:28px; font-weight:700; color:#111827;">
+                {value}
+            </div>
         </div>
-        <div class="kpi-value">{value}</div>
+
+        <div style="
+            background:#ede9fe;
+            color:#6D28D9;
+            padding:10px;
+            border-radius:12px;
+            font-size:18px;
+        ">
+            {icon}
+        </div>
     </div>
-    """
+    """, unsafe_allow_html=True)
 
 c1, c2, c3, c4 = st.columns(4)
 
